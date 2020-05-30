25ab855e346678295a4390350029953f9d702471
Mete Ercan Pakdil <mete@pakdil.org>
2020-05-30 23:49:04 +0100

    remove spaces in sheet 100s (#2)

--- test/Gu.PaftaBulucu.WebApi.Tests/SheetsControllerTests.cs
+++ test/Gu.PaftaBulucu.WebApi.Tests/SheetsControllerTests.cs
@@ -22,7 +22,7 @@ namespace Gu.PaftaBulucu.WebApi.Tests
             var response = await lambdaFunction.FunctionHandlerAsync(request, context);
 
             Assert.Equal(200, response.StatusCode);
-            Assert.Equal("{\"name\":\"Kırşehir-I 31-a-20-d-4-a\",\"lat\":39.80625,\"lon\":33.7,\"scale\":1}", response.Body);
+            Assert.Equal("{\"name\":\"Kırşehir-I31-a-20-d-4-a\",\"lat\":39.80625,\"lon\":33.7,\"scale\":1}", response.Body);
         }
         
         [Fact]
@@ -36,7 +36,7 @@ namespace Gu.PaftaBulucu.WebApi.Tests
             var response = await lambdaFunction.FunctionHandlerAsync(request, context);
 
             Assert.Equal(200, response.StatusCode);
-            Assert.Equal("[{\"name\":\"Mardin-N 47-b-17\",\"lat\":37.3,\"lon\":41.8,\"scale\":10}]", response.Body);
+            Assert.Equal("[{\"name\":\"Mardin-N47-b-17\",\"lat\":37.3,\"lon\":41.8,\"scale\":10}]", response.Body);
         }
 
 
